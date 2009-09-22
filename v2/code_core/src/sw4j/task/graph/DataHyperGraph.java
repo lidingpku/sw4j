0c7df7a03f398931b230ae0e658db47a79ff3b3d
huangr3 <huangr3@23d186f6-0d78-11de-b321-f573fdaff03e>
2009-09-22 05:25:47 +0000

    Use graphViz API to output .dot file

--- v2/code_core/src/sw4j/task/graph/DataHyperGraph.java
+++ v2/code_core/src/sw4j/task/graph/DataHyperGraph.java
@@ -33,6 +33,10 @@ package sw4j.task.graph;
  * 
  */
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -47,6 +51,10 @@ import sw4j.util.DataPVHMap;
 import sw4j.util.ToolRandom;
 import sw4j.util.ToolSafe;
 import sw4j.util.ToolString;
+
+import org.kohsuke.graphviz.Edge;
+import org.kohsuke.graphviz.Graph;
+import org.kohsuke.graphviz.Node;
 
 public class DataHyperGraph {
 	public static int DEFAULT_WEIGHT = 0;
@@ -507,6 +515,113 @@ public class DataHyperGraph {
 	}
 	
 	/**
+	 * export data using graphviz API format
+	 * 
+	 * @param reference  - the referenced hypergraph that record the provenance metadata for each hyperedge
+	 * @return
+	 */
+//	public Graph data_export_graphvizAPI(){
+	public Graph data_export_graphvizAPI(Map<Integer,String> map_node_id_input, Map<Integer,Properties> map_node_params, Map<DataHyperEdge,Properties> map_edge_params){
+		//list all nodes
+		Graph g= new Graph();
+		
+		Set<Integer> nodes = getNodes();
+		
+		//prepare map_node_id
+		HashMap <Integer,Node> map_node_id = new HashMap<Integer,Node> ();
+		for(Integer node: nodes){
+			String id = null;
+			if (null!= map_node_id_input)
+				id = map_node_id_input.get(node);
+			if (null==id)
+				id="x_"+node;
+			Node node1= new Node();
+			node1.id(id);
+			map_node_id.put(node,node1);
+		}
+		
+		{
+			//print nodes
+			for(Integer node: nodes){
+
+				Node label = map_node_id.get(node);
+				if (null!=map_node_params){
+					Properties prop= map_node_params.get(node);
+					if (ToolSafe.isEmpty(prop)){
+						prop= new Properties();
+					}
+					prop.put("shape", "box");
+
+					for (Object key :prop.keySet()){
+							label.attr(key.toString(),prop.get(key).toString());
+					}	
+				}
+			g.node(label);	
+			}
+		}
+		{
+			//print arcs
+			Iterator<DataHyperEdge> iter= this.getEdges().iterator();
+			int edgeid=1;
+			Node label;
+			while (iter.hasNext()){
+				DataHyperEdge edge = iter.next();
+				
+				String e = "a_"+edgeid;
+				edgeid++;
+				Node node2= new Node();
+				node2.id(e);
+				
+				if (null!=map_edge_params){
+					Properties prop= map_edge_params.get(edge);
+					if (ToolSafe.isEmpty(prop)){
+						prop= new Properties();
+					}
+					prop.put("shape", "diamond");
+					for (Object key :prop.keySet()){
+						node2.attr(key.toString(),prop.get(key).toString());
+					}
+				}
+				g.node(node2);
+				
+				
+				Integer output= edge.getOutput();
+				label = map_node_id.get(output);
+				Edge edge1= new Edge(label, node2);
+				g.edge(edge1);
+				Iterator<Integer> iter_input = edge.getInputs().iterator();
+				while (iter_input.hasNext()){
+					Integer input= iter_input.next();
+					label = map_node_id.get(input);
+					Edge edge2= new Edge(node2, label);
+					g.edge(edge2);
+				}
+			}
+			
+		}
+		
+//		rank=same;
+//		for(Integer leaf: this.getAxioms()){
+//			String label = map_node_id.get(leaf);
+//			ret += String.format(" \"%s\"",  label );			
+//		}
+
+//		Node node1= new Node();
+//		node1.id("I");
+//		Node node2= new Node();
+//		node2.id("you");
+//		node1.attr("shape", "diamond");
+//		node2.attr("shape","box");
+//		Edge edge= new Edge(node1,node2);
+//		edge.attr("label", "love");
+//		g.node(node1);
+//		g.node(node2);
+//		g.edge(edge);
+		return g;
+	}
+
+	
+	/**
 	 * export data in to text based exchange format
 	 * 
 	 * @param reference  - the referenced hypergraph that record the provenance metadata for each hyperedge
@@ -597,6 +712,20 @@ public class DataHyperGraph {
 		ret +="\n}\n";
 		return ret;
 	}
+	
+	public static void main(String[] args) throws IOException{
+	
+	DataHyperGraph g= new DataHyperGraph();
+	Graph g1= g.data_export_graphvizAPI(null,null,null);
+	File f=new File("docs/test.dot");
+    FileOutputStream fop=new FileOutputStream(f);
+	g1.writeTo(fop);
+    
+}
+
+
+	
+	
 	/**
 	 * import data 
 	 * 
@@ -735,4 +864,8 @@ public class DataHyperGraph {
 
 		return lg;
 	}
+	
+	
+	
+	
 }
