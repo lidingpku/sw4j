da3a56f4b1d18d7738950e7b1f0b0cf0a3874eeb
lidingpku <lidingpku@23d186f6-0d78-11de-b321-f573fdaff03e>
2009-10-02 12:52:11 +0000

    improve gzip outputstream performance http://bugs.sun.com/bugdatabase/view_bug.do;jsessionid=15171f31aada88399a136fc29745?bug_id=6623164

--- trunk/v2/code_core/src/sw4j/util/ToolIO.java
+++ trunk/v2/code_core/src/sw4j/util/ToolIO.java
@@ -29,6 +29,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 package sw4j.util;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -172,7 +173,9 @@ public class ToolIO {
 
 			OutputStream out = new FileOutputStream(f,bAppend);
 			if (!bAppend && bGzip){
-				out = new GZIPOutputStream(out);
+				// this is used for performance improvement
+				//http://bugs.sun.com/bugdatabase/view_bug.do;jsessionid=15171f31aada88399a136fc29745?bug_id=6623164
+				out = new GZIPOutputStream(new BufferedOutputStream(out));
 			}
 			return out;
 			
